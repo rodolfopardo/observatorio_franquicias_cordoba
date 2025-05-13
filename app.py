d8478041afece7c5e8d31bc5f30f8c30ff637127
Rodolfo Pardo <rodypardo@gmail.com>
2025-05-13 12:12:34 -0300

    Update app.py
    Actualizacion de base nueva

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ st.sidebar.markdown(f"👤 Sesión iniciada como: `{st.session_state.get('user',
 
 @st.cache_data
 def cargar_datos():
-    df = pd.read_csv("https://drive.google.com/file/d/19yEmueLePXiPZVV9fYYQWbLhMP9N6AzO/")
+    df = pd.read_csv("https://drive.google.com/uc?export=download&id=19yEmueLePXiPZVV9fYYQWbLhMP9N6AzO")
     df['title'] = df['title'].astype(str)
     df['addressPreview'] = df['addressPreview'].astype(str)
     df = df[df['addressPreview'].str.contains(r'c[oó]rdoba', case=False, na=False)]
